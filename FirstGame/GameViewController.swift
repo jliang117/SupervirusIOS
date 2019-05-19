452ce71db8e0326deaa717e1c5b306b3d6eb7274
jliang117 <jliang117@users.noreply.github.com>
2019-05-18 22:20:07 -0400

    .gitignore fix

--- FirstGame/GameViewController.swift
+++ FirstGame/GameViewController.swift
@@ -13,33 +13,13 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
---
+++ FirstGame/Virus.swift
@@ -0,0 +1,23 @@
+//
+//  Virus.swift
+//  FirstGame
+//
+//  Created by Jimmy Liang on 5/14/19.
+//  Copyright © 2019 Jimmy Liang. All rights reserved.
+//
+
+import SpriteKit
+
+class Virus : SKSpriteNode
+{
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    required override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+    }
+    
+   
+}
