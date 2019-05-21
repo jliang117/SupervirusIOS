96ed64668423450207df12bb4af317613bd4e80c
jliang117 <jliang117@users.noreply.github.com>
2019-05-21 16:23:34 -0400

    make monster node

--- FirstGame/Nodes/MonsterVirus.swift
+++ FirstGame/Nodes/MonsterVirus.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2019 Jimmy Liang. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class MonsterVirus : Virus
+{
+    lazy private var virusImage: SKTexture = {
+        let virusImage: SKTexture = SKTexture.init(imageNamed: "heroVirus")
+        return virusImage
+    }()
+    
+    lazy private var radius: CGFloat = {
+        let startSize = virusImage.size()
+        return startSize.width/2
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(radius: CGFloat, startPos: CGPoint, imageNamed: String) {
+        super.init(radius: radius, startPos: startPos, imageNamed: imageNamed)
+        
+        let path = CGMutablePath()
+        path.addArc(center: CGPoint.zero, radius: radius, startAngle: 0, endAngle: 2 * CGFloat(Double.pi), clockwise: true)
+        let physics = SKPhysicsBody(circleOfRadius: radius)
+        physics.allowsRotation = false
+        physics.affectedByGravity = false
+        physicsBody = physics
+    }
+    
+}
